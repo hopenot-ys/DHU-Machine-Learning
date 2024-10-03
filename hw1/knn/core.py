7f615f416f0f7e6bff84582241f2ee394d181b8e
hopenot-ys <iris0907x@gmail.com>
2024-10-03 12:50:19 +0800

    feat: knn algorithm

--- hw1/knn/core.py
+++ hw1/knn/core.py
@@ -1,12 +1,9 @@
-# 算法核心
-from sklearn import datasets
-from collections import Counter  # 为了做投票
-from sklearn.model_selection import train_test_split
+from collections import Counter
 import numpy as np
 
 
 class KNN:
-    def __init__(self, X, Y, K):
+    def __init__(self, X, Y, K, P):
         '''
         X: Traning Dataset
         Y: Corresponding Labels
@@ -15,13 +12,30 @@ class KNN:
         self.X = X
         self.Y = Y
         self.K = K
+        self.P = P
 
-    def classify(x):
+    def _minkowski_distance(x1, x2, p):
+        return np.linalg.norm(x1 - x2, ord=p)
+
+    def _get_k_nearest(self, x, dis_func):
+        distances = [dis_func(i, x, self.P) for i in self.X]
+        sorted_indices = np.argsort(distances)
+        return sorted_indices[:self.K]
+
+    def _classify(self, x, dis_func):
         '''
         x: an input data point
         return: the classifed result for the input x
         '''
-        
+        k_lables = self.Y[self._get_k_nearest(x, dis_func)]
+        return Counter(k_lables).most_common(1)[0][0]
 
-    def regression():
-        ...
+    def _regress(self, x, dis_func):
+        k_values = self.Y[self._get_k_nearest(x, dis_func)]
+        return np.mean(k_values)
+
+    def knn_classify(self, x):
+        return self._classify(x, KNN._minkowski_distance)
+
+    def knn_regress(self, x):
+        return self._regress(x, KNN._minkowski_distance)
